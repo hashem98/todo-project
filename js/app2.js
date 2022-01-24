1452fbc5bc9f64ef0cde347574776f8a6029eb8f
Hashem Smadi <43494651+hashem98@users.noreply.github.com>
2022-01-24 15:54:33 +0200

    last fix

--- js/app2.js
+++ js/app2.js
@@ -1,39 +1,40 @@
 let qus = [];
+let flag=true;
 function fillQuestions() {
     qus[0] = prompt("Do you have a background in technology?").toLowerCase();
-    if(qus[0]=="yes"||qus[0]=="no"||qus[0]=="y"||qus[0]=="n")
-       log.console("its ok");
+    if(qus[0]=="yes"||qus[0]=="no"||qus[0]=="y"||qus[0]=="n"){
+        }
     else{
-        while (true){
+        while (flag){
             qus[0] = prompt("your answer must be (yes or no , Y or N)").toLowerCase();
             if(qus[0]=="yes"||qus[0]=="no"||qus[0]=="y"||qus[0]=="n"){
-            break; }
+            flag=false; }
         } 
     
     }
     // if (qus[0] == "")
     //     qus[0] = "invalid"
     qus[1] = prompt("Are you unemployed?").toLowerCase();
-    if(qus[1]=="yes"||qus[1]=="no"||qus[1]=="y"||qus[1]=="n")
-        log.console("its ok");
+    if(qus[1]=="yes"||qus[1]=="no"||qus[1]=="y"||qus[1]=="n"){
+        }
     else{
-        while (true){
+        while (flag){
             qus[1] = prompt("your answer must be (yes or no , Y or N)").toLowerCase();
             if(qus[1]=="yes"||qus[1]=="no"||qus[1]=="y"||qus[1]=="n"){
-            break; }
+                flag=false; }
         } 
     
     }
     // if (qus[1] == "")
     //     qus[1] = "invalid"
     qus[2] = prompt("Are you under 35?").toLowerCase();
-    if(qus[2]=="yes"||qus[2]=="no"||qus[1]=="y"||qus[1]=="n")
-       log.console("its ok");
+    if(qus[2]=="yes"||qus[2]=="no"||qus[1]=="y"||qus[1]=="n"){
+       }
     else{
-        while (true){
+        while (flag){
             qus[2] = prompt("your answer must be (yes or no , Y or N)").toLowerCase();
             if(qus[2]=="yes"||qus[2]=="no"||qus[2]=="y"||qus[2]=="n"){
-            break; }
+                flag=false; }
         } 
     
     }
