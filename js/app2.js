c13b52cdace9938b401b051c8fe3caee136aa26d
Hashem Smadi <43494651+hashem98@users.noreply.github.com>
2022-01-24 15:01:05 +0200

    Update app2.js

--- js/app2.js
+++ js/app2.js
@@ -1,7 +1,9 @@
 let qus = [];
 function fillQuestions() {
     qus[0] = prompt("Do you have a background in technology?").toLowerCase();
-    if(qus[0]!="yes"||qus[0]!="no"||qus[0]!="y"||qus[0]!="n"){
+    if(qus[0]=="yes"||qus[0]=="no"||qus[0]=="y"||qus[0]=="n")
+    continue;
+    else{
         while (true){
             qus[0] = prompt("your answer must be (yes or no , Y or N)").toLowerCase();
             if(qus[0]=="yes"||qus[0]=="no"||qus[0]=="y"||qus[0]=="n"){
@@ -12,7 +14,9 @@ function fillQuestions() {
     // if (qus[0] == "")
     //     qus[0] = "invalid"
     qus[1] = prompt("Are you unemployed?").toLowerCase();
-    if(qus[1]!="yes"||qus[1]!="no"||qus[1]!="y"||qus[1]!="n"){
+    if(qus[1]=="yes"||qus[1]=="no"||qus[1]=="y"||qus[1]=="n")
+    continue;
+    else{
         while (true){
             qus[1] = prompt("your answer must be (yes or no , Y or N)").toLowerCase();
             if(qus[1]=="yes"||qus[1]=="no"||qus[1]=="y"||qus[1]=="n"){
@@ -23,7 +27,9 @@ function fillQuestions() {
     // if (qus[1] == "")
     //     qus[1] = "invalid"
     qus[2] = prompt("Are you under 35?").toLowerCase();
-    if(qus[2]!="yes"||qus[2]!="no"||qus[2]!="y"||qus[2]!="n"){
+    if(qus[2]=="yes"||qus[2]=="no"||qus[1]=="y"||qus[1]=="n")
+    continue;
+    else{
         while (true){
             qus[2] = prompt("your answer must be (yes or no , Y or N)").toLowerCase();
             if(qus[2]=="yes"||qus[2]=="no"||qus[2]=="y"||qus[2]=="n"){
